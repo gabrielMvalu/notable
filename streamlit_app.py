19f7af441997966a1b33ae91fc3b3b82a3281f63
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-20 21:11:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,34 @@
 import streamlit as st
 from PIL import Image
+import pandas as pd
 
 def main():
     st.title("Aplicația mea Streamlit")
 
-    # Sidebar pentru încărcarea și afișarea logo-ului
-    st.sidebar.title("Rearanjare Tabel P. Financiar")
-    logo_path = "LogoSTR.PNG"  # Presupunem că logo-ul este în același director cu scriptul
+    # Sidebar pentru încărcarea și afișarea logo-ului și textului
+    st.sidebar.title("Încărcare Document")
+    logo_path = "LogoSTR.PNG"
     try:
         logo = Image.open(logo_path)
         st.sidebar.image(logo, use_column_width=True)
     except IOError:
         st.sidebar.error("Eroare la încărcarea logo-ului.")
+    st.sidebar.markdown("<small>© CASTEMILL SRL</small>", unsafe_allow_html=True)
 
-    # Adăugarea textului "© CASTEMILL SRL" în sidebar
-    st.sidebar.markdown("<small>© Castemill S.R.L.</small>", unsafe_allow_html=True)
-    
     # Încărcarea fișierului
     uploaded_file = st.file_uploader("Încarcă documentul XLSX aici", type="xlsx", accept_multiple_files=False)
 
-    
-   # Funcție pentru verificarea existenței foii "P. FINANCIAR"
-def verifica_foaia_p_financiar(uploaded_file):
-    try:
-        # Citim fișierul încărcat direct într-un DataFrame pandas
-        df = pd.read_excel(uploaded_file, sheet_name="P. FINANCIAR")
-        return df, True
-    except ValueError:
-        # Dacă foaia nu există, întoarcem False
-        return None, False
+    # Funcție pentru verificarea existenței foii "P. FINANCIAR"
+    def verifica_foaia_p_financiar(uploaded_file):
+        try:
+            # Citim fișierul încărcat direct într-un DataFrame pandas
+            df = pd.read_excel(uploaded_file, sheet_name="P. FINANCIAR")
+            return df, True
+        except ValueError:
+            # Dacă foaia nu există, întoarcem False
+            return None, False
 
- # Butoane pentru generarea tabelelor
+    # Butoane pentru generarea tabelelor
     if st.button("Generează Tabel 1"):
         if uploaded_file is not None:
             df, foaie_gasita = verifica_foaia_p_financiar(uploaded_file)
@@ -53,7 +51,5 @@ def verifica_foaia_p_financiar(uploaded_file):
         else:
             st.error("Te rog să încarci un fișier pentru a genera Tabelul 2.")
 
-
 if __name__ == "__main__":
     main()
-
