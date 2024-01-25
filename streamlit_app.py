5e5c2b499d89b8879b9983442866c0ce725e3b8f
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-25 23:20:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -207,7 +207,7 @@ def main():
                 um.append("buc")
                 cantitate.append(df_filtrat.iloc[i-1, 11])  # Adjust the index as necessary
                 pret_unitar.append(df_filtrat.iloc[i-1, 3])
-                valoare_totala.append(df_filtrat.iloc[i-1, 4])
+                valoare_totala.append(df_filtrat.iloc[i-1, 6])
                 nr_crt_counter += 1
         
             # Add other specific entries after processing all items
